36395f5b50b558001036ea05820cf4bd88c7a1ea
mynacche <chexingyou@gmail.com>
2013-11-28 17:04:07 +0800

    Create simpleproxy.js

---
+++ simpleproxy.js
@@ -0,0 +1,73 @@
+(function(){
+	function SimpleProxy() {
+		this.signed = false;
+		this.error = false;
+		this.events = [];
+		this.count = 0;
+		this.callback = null;
+	}
+	SimpleProxy.prototype.sign = function(events, callback) {
+		var isArray = (Object.prototype.toString.call(events) === "[object Array]"), 
+			args = [].slice.call(arguments),
+			events = isArray ? events : args.slice(0,args.length - 1), 
+			count = events.length;
+
+		for ( var i = 0; i < count; i++) {
+			this.events.push({
+				key : events[i],
+				value : null
+			});
+		}
+		this.count = count;
+		this.callback = isArray ? callback : args[args.length - 1];
+		this.signed = true;
+	};
+	SimpleProxy.prototype.emit = function(event, data) {
+		var signed = this.signed,
+			error = this.error,
+			events = this.events, 
+			callback = this.callback, 
+			count = events.length,
+			isFunction = Object.prototype.toString.call(callback) === '[object Function]';
+			
+		if (!signed) {
+			throw 'error: proxy need to be signed first.';
+		}
+		if(error){
+			return;
+		}
+		if(event === 'err' || event === 'error'){
+			this.error = true;
+			if (isFunction) {
+				return callback.call(this, data);
+			}else{
+				throw 'error: ' + data;
+			}
+		}
+		for ( var i = 0; i < count; i++) {
+			if (event == events[i].key) {
+				events[i].value = data;
+			}
+		}
+		this.count--;
+		if (this.count == 0) {
+			var values = [null];
+			for ( var i = 0; i < count; i++) {
+				values.push(events[i].value);
+			}
+			if (isFunction) {
+				callback.apply(this, values);
+			}
+		}
+	};
+	SimpleProxy.prototype.err = function(err){
+		this.emit('err',err);
+	};
+	
+	if (typeof module !== 'undefined' && module
+			&& typeof module.exports !== 'undefined') {
+		module.exports = SimpleProxy;
+	} else {
+		this.SimpleProxy = SimpleProxy;
+	}
+})(undefined);
